4ad52d9d58c54220c54ac3c6ea94f6dd5b3294c9
Jessica Hekman <jphekman@cabeswater.local>
2024-02-22 20:32:56 -0500

    Error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -14,6 +14,7 @@ const FormSchema = z.object({
 });
  
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
 
@@ -27,16 +28,55 @@ export async function createInvoice(formData: FormData) {
     });
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0]; // "YYYY-MM-DD"
-    
-    await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
 
+    try {
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+  } catch (error) {
+      return { message: 'Failed to Update Invoice: ${error}' };
+  } 
     // update client cache
     revalidatePath('/dashboard/invoices');
 
     redirect('/dashboard/invoices');
 
 }
-							   
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+    try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch (error) {
+    return {
+	message: 'Failed to update invoice: ${error}'
+    };
+  }
+    
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+
+    try {
+	await sql`DELETE FROM invoices WHERE id = ${id}`;
+    } catch (error) {
+	return {
+	message: 'Failed to delete invoice: ${error}'
+	};
+    };
+  revalidatePath('/dashboard/invoices');
+}
