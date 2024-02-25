1b0a9aea02daa56cb74cb8eb3439ffdf281fd265
Jessica Hekman <jphekman@cabeswater.local>
2024-02-24 21:04:00 -0500

    Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
+import { Metadata } from 'next';
 import { inter } from '@/app/ui/fonts';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Ponies Ponies Ponies',
+    default: 'Ponies Ponies Ponies',
+  },
+  description: 'Model horse showing made easy.',
+  metadataBase: new URL('https://nextjs-dashboard-sepia-seven-46.vercel.app/'),
+};
 
 export default function RootLayout({
   children,
