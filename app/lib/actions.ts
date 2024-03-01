58569e4beace4cf0d0c9722a1a32e645ed3b3613
Jessica Hekman <jphekman@cabeswater.local>
2024-03-01 18:29:03 -0500

    removed Next Auth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,8 +4,6 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-import { signIn } from '@/auth'; // TODO DELETEME?
-import { AuthError } from 'next-auth'; // TODO DELETEME?
  
 const FormSchema = z.object({
   id: z.string(),
@@ -120,21 +118,3 @@ export async function deleteInvoice(id: string) {
   revalidatePath('/dashboard/invoices');
 }
 
-export async function authenticate(
-  prevState: string | undefined,
-  formData: FormData,
-) {
-  try {
-    await signIn('credentials', formData);
-  } catch (error) {
-    if (error instanceof AuthError) {
-      switch (error.type) {
-      case 'CredentialsSignin':
-        return 'Invalid credentials.';
-      default:
-        return 'Something went wrong.';
-      }
-    }
-    throw error;
-  }
-}
